04e980775519f9bb089e3697bc49ad34561db90a
William Chou <williamchou@starlux-airlines.com>
2020-02-21 17:16:10 +0800

    Add main files

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={ () => {this.setState({ value: '❌' }) }}
+      >
+        { this.state.value }
+      </button>
+    );
+  }
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={ i } />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
