991506e42114a62c3ecba8e6ade3799817c90bf0
Memo Chou <memochou1993@gmail.com>
2020-02-21 22:14:41 +0800

    Modify Square and Board components

--- src/index.js
+++ src/index.js
@@ -3,29 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={ () => {this.setState({ value: '❌' }) }}
+        onClick={() => { this.props.onClick() }}
       >
-        { this.state.value }
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = '❌';
+    this.setState({ squares });
+  }
+
   renderSquare(i) {
-    return <Square value={ i } />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
