d00888da80a6ea2f7408953ad1583e2bb0550073
Memo Chou <memochou1993@gmail.com>
2020-02-22 01:13:13 +0800

    Add history

--- src/index.js
+++ src/index.js
@@ -14,11 +14,46 @@ const Square = (props) => {
 }
 
 class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      squares: Array(9).fill(null),
+      history: [{
+        squares: Array(9).fill(null),
+      }],
       move: 0,
     };
   }
@@ -46,67 +81,40 @@ class Board extends Component {
   }
 
   handleClick(i) {
-    const squares = [...this.state.squares];
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = [...current.squares];
 
     if (this.judge(squares) || squares[i]) {
       return;
     }
 
-    squares[i] = this.state.move % 2 ? 'X' : 'O'; // computed?
+    squares[i] = this.state.move % 2 ? 'X' : 'O';
 
     this.setState({
-      squares,
+      history: [...this.state.history, { squares }],
       move: this.state.move + 1,
     });
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
   render() {
-    const winner = this.judge(this.state.squares);
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = this.judge(current.squares);
     const status = winner
       ? `Winner: ${winner}`
       : `Next player: ${this.state.move % 2 ? 'X' : 'O'}`;
 
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends Component {
-  render() {
-    return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => { this.handleClick(i) }}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
